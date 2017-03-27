a754acdfc2550464d0e3dd5e4f90c6076bcdaa0a
StayHungryStayFoolish <bonismo@hotmail.com>
2017-03-27 09:54:38 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by bonismo
+ * 14/9/1 上午11:02
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,World！");
+
+        System.out.println("你好，世界！");
+    }
+}
---
+++ src/main/java/Human.java
@@ -0,0 +1,20 @@
+/**
+ * Created by bonismo
+ * 17/3/25 下午5:46
+ */
+public class Human {
+    public static void main(String[] args) {
+
+        System.out.println("Human");
+
+    }
+}
+
+class Boxed{
+    public static void main(String[] args) {
+        int i = 100;
+        Integer integer = 1000;
+        System.out.println(integer);
+        System.out.println(i);
+    }
+}
